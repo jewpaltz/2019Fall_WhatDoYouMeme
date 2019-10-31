6e717cd12a123f91f8f5b283eacdd8a84086cd91
Moshe Plotkin <mplotkin@jewpaltz.com>
2019-10-31 12:18:31 -0400

    Some real functionality

---
+++ client/src/models/Game.js
@@ -0,0 +1,30 @@
+export const Game_Server = {
+    Players: [],
+    Picture_Deck: [],
+    Caption_Deck: [],
+    Dealer: -1,
+    Captions_In_Play: [], // strings
+    Picture_In_Play: "",
+    Caption_Chosen: -1
+}
+
+export const Game_Client = {
+    Players: [
+        { name: "Moshe", points: 0 },
+        { name: "Bernie", points: 0 },
+        { name: "Donald", points: 0 },
+        { name: "Andrew", points: 0 },
+    ],
+    Dealer: -1,
+    Captions_In_Play: [], // strings
+    Picture_In_Play: "",
+    Caption_Chosen: -1
+}
+
+export var My_Captions = [];
+
+export class Player {
+    name;
+    points;
+    captions;
+}
